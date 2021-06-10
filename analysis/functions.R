4648dac6245562518820b0e3f7522acd0770908e
Konstantinos Patelis <kos_patel@yahoo,gr>
2021-06-10 02:56:27 +0200

    Upload current progress

--- analysis/functions.R
+++ analysis/functions.R
@@ -1,4 +1,9 @@
 
+removeWords <- function(str, stopwords) {
+  x <- unlist(strsplit(str, " "))
+  paste(x[!x %in% stopwords], collapse = " ")
+}
+
 # This function will check to see if the data is available in the project directory
 # If not, it will download and store it in the /data folder
 # Then it will provide options to load the data
@@ -41,7 +46,7 @@ read_text <- function(file_name, prows = 0.15, nrows = NULL, seed = 1234) {
 # training and testing sets based on a specified proportion
 # seed is required for reproducibility
 
-split_text <- function(..., names = NULL, split = NULL, seed = 1234) {
+split_text <- function(..., names = NULL, split = NULL, prop = .1, seed = 1234) {
   
   args <- list(...)
   
@@ -67,11 +72,18 @@ split_text <- function(..., names = NULL, split = NULL, seed = 1234) {
   
   set.seed(seed)
   
-  distr <- map(l, rbinom, size = 1, prob = split)
+  proport <- map(l, rbinom, size = 1, prob = prop)
+  proport <- map(proport, as.logical)
+  
+  texts_prop <- map2(texts, proport, `[`)
+  
+  ll <- map(texts_prop, length)
+  
+  distr <- map(ll, rbinom, size = 1, prob = split)
   distr <- map(distr, as.logical)
   not_distr <- map(distr, `!`)
   
-  train <- map2(texts, distr, `[`)
+  train <- map2(texts_prop, distr, `[`)
   train <- map(train, as_tibble)
 
   names <- as.list(names)
@@ -84,7 +96,7 @@ split_text <- function(..., names = NULL, split = NULL, seed = 1234) {
 
   train <- do.call(bind_rows, train)
   
-  test <- map2(texts, not_distr, `[`)
+  test <- map2(texts_prop, not_distr, `[`)
   test <- map(test, as_tibble)
   
   for (i in 1:length(test)) {
@@ -101,7 +113,7 @@ split_text <- function(..., names = NULL, split = NULL, seed = 1234) {
   
 }
 
-make_ngram <- function(data, n = 4, id = id, value = value, word_len_max = 18, top_word_freq = NULL, profanity_filter = NULL) {
+make_ngram <- function(data, n = 6, id = id, value = value, word_len_max = 18, top_word_freq = NULL, profanity_filter = NULL, train = TRUE) {
   id_q <- deparse(substitute(id))
   value_q <- deparse(substitute(value))
   word_vec <- paste0("word_", 1:n)
@@ -115,7 +127,7 @@ make_ngram <- function(data, n = 4, id = id, value = value, word_len_max = 18, t
   
   id_leftover <- data_dt[, get(id_q)]
   
-  if (!is.null(top_word_freq)) {
+  if (!is.null(top_word_freq) & (train)) {
     
     word_freq <- unnest_tokens(data_dt, output = word, input = value, token = "words")
     
@@ -173,7 +185,7 @@ make_ngram <- function(data, n = 4, id = id, value = value, word_len_max = 18, t
       
     }
     
-    if (!is.null(top_word_freq)) {
+    if (!is.null(top_word_freq) & (train)) {
       
       ngram_final <- ngram_final[get(i) %in% freq_words]
       
@@ -192,6 +204,9 @@ kn_smoothing <- function(data, discount_factor = .75) {
   # dt <- copy(as.data.table(ngram))
   n <- length(dt) - 2
   word_vec <- paste0("word_", 1:n)
+  id_vec <- paste0("id_", 1:n)
+  prob_vec <- paste0("prob_", 1:n)
+  keep_cols <- c(id_vec, prob_vec)
   
   ngram_list <- map(1:n, ~ dt[!is.na(get(word_vec[.x])), .(count = sum(N)), by = c(word_vec[.x:n])])
   list_name <- paste0("order_", n:1)
@@ -223,28 +238,12 @@ kn_smoothing <- function(data, discount_factor = .75) {
     temp[, (ccount[i]) := (get(ccount_nom[i]) - discount_factor) / get(ccount_denom[i])]
     temp <- unique(temp[, .SD, .SDcols = c(word_vec[(i + 1):n], ccount_nom[i], ccount_denom[i], ccount[i])])
     
-    ngram_list[[list_name[(i + 1)]]] <- merge(ngram_list[[list_name[(i + 1)]]], 
-                                              temp, 
+    ngram_list[[list_name[(i + 1)]]] <- merge(ngram_list[[list_name[(i + 1)]]],
+                                              temp,
                                               by = c(word_vec[(i + 1):n]),
-                                              all.x = TRUE, 
+                                              all.x = TRUE,
                                               all.y = FALSE)
   }
-  
-  # temp_1 <- ngram_list[[list_name[1]]][, (ccount_nom[1]) := .N, by = c(word_vec[2:n])]
-  # 
-  # temp_2 <- unique(temp_1[, .SD, .SDcols = c(word_vec[1:(n-1)])])
-  # temp_2 <- temp_2[, (ccount_denom[1]) := .N, by = c(word_vec[2:(n-1)])][, .SD, .SDcols = c(word_vec[2:(n-1)], ccount_denom[1])]
-  # temp_2 <- unique(temp_2)
-  # 
-  # temp <- merge(temp_1, temp_2, by = c(word_vec[2:(n-1)]), all.x = TRUE)
-  # temp[, (ccount[1]) := (get(ccount_nom[1]) - .75) / get(ccount_denom[1])]
-  # temp <- unique(temp[, .SD, .SDcols = c(word_vec[2:n], ccount_nom[1], ccount_denom[1], ccount[1])])
-  # 
-  # ngram_list[[list_name[2]]] <- merge(ngram_list[[list_name[2]]], 
-  #                                     temp, 
-  #                                     by = c(word_vec[2:n]),
-  #                                     all.x = TRUE, 
-  #                                     all.y = FALSE)
   
   # Recursive calculations
   
@@ -274,17 +273,41 @@ kn_smoothing <- function(data, discount_factor = .75) {
     
   }
   
-  ngram_list[[list_name[n]]][, probability := count / sum(count)] 
+  ngram_list[[list_name[n]]][, probability := count / sum(count)]
   
   for (i in 1:n) {
     
     ngram_list[[list_name[i]]] <- ngram_list[[list_name[i]]][, .SD, .SDcols = c(word_vec[i:n], "probability")]
+    setnames(ngram_list[[list_name[i]]], old = "probability", new = prob_vec[i])
+    
+    if (i == 1) {
+      
+      table <- ngram_list[[list_name[1]]]
+      
+    } else {
+      
+      table <- merge(table, ngram_list[[list_name[i]]], by = word_vec[i:n])
+      
+    }
     
   }
   
-  dt <- do.call(rbind, args = c(ngram_list, fill = TRUE))
+  words <- as.data.table(unique(unlist(ngram[, ..word_vec])))
+  setnames(words, old = "V1", new ="word")
+  words[, id := .I]
   
-  # ngram_list
-  dt
+  for (i in 1:n) {
+    
+    table <- merge(table, words, by.x = word_vec[i], by.y = "word")
+    
+    setnames(table, old = "id", new = id_vec[i])
+    
+  }
+  
+  table <- table[, .SD, .SDcols = keep_cols]
+  
+  l <- list(lookup = words, ngram_table = table)
+  
+  l
   
 }
